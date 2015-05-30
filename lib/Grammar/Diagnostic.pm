fc0938c901d42af34d896904842ff27246b3eb6a
David Farrell <davidnmfarrell@gmail.com>
2015-05-30 12:25:25 -0400

    initial release

---
+++ lib/Grammar/Diagnostic.pm
@@ -0,0 +1,49 @@
+class Grammar::Diagnostic:ver<0.01>
+{
+  # this array tracks matching start/end match object pairs like =over & =back
+  has @.match_pair_stack = Array.new;
+
+  method push_match (Match:D $match)
+  {
+    @.match_pair_stack.push($match);
+  }
+
+  method pop_match ()
+  {
+    @.match_pair_stack.pop;
+  }
+
+  # retuns a list of unmatched pair diagnosis msgs
+  method unmatched_diagnostics ()
+  {
+    my @unmatched = Array.new;
+
+    for @.match_pair_stack -> $match
+    {
+      my $location = self.match_location($match);
+      @unmatched.push("Did not find matching token for '{self.match_text($match)}' line {$location<line>}, column {$location<column>}");
+    }
+
+    return @unmatched;
+  }
+
+  method print_diagnostics ()
+  {
+    .say for self.unmatched_diagnostics;
+  }
+
+  # translates match object to/from into line # and column #
+  method match_location (Match:D $match)
+  {
+    my $substring = $match.orig.substr(0, $match.from);
+    my @lines = $substring.split(/\n/);
+    return { line => @lines.elems, column => @lines[*-1].chars };
+  }
+
+  # returns the match text
+  method match_text (Match:D $match)
+  {
+    return $match.orig.substr($match.from, $match.to - $match.from);
+  }
+}
+
---
+++ t/Debug.t
@@ -0,0 +1,43 @@
+use Test;
+use lib 'lib';
+
+plan 4;
+
+use Grammar::Diagnostic; pass "Import Grammar::Diagnostic";
+
+ok my $diag = Grammar::Diagnostic.new, 'constructor';
+
+subtest
+{
+  my $input_string = "Perl 6\nis a new language\nfor doing\ncool things";
+  my $match = $input_string.match(/(doing)/);
+
+  # match location
+  ok my $match_location = $diag.match_location($match), 'get match location';
+  is $match_location<line>,   3, 'match location line number is correct';
+  is $match_location<column>, 4, 'match location column number is correct';
+
+  # match_text
+  ok my $match_text = $diag.match_text($match), 'get match text';
+  is $match_text, 'doing', 'match text matches expected';
+}, 'Match location and text';
+
+subtest
+{
+  my $input_string = q:to/END/;
+  =begin
+
+  some text
+
+  =begin
+
+  some more text
+
+  =end
+  END
+
+  my $match = $input_string.match(/(^^\=begin)/);
+
+  ok $diag.push_match($match), 'push the match onto the stack';
+  $diag.print_diagnostics;
+}, 'Match pairs and diagnostics';
